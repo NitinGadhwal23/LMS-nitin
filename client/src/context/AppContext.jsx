8ed889e6d8081e751165fbc59f75d71f433ed92a
“[Deepak <[deepaktomar3122003@gmail.com]>
2025-01-27 23:19:19 +0530

    LMS Commit

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,20 @@
+ import { createContext, useEffect, useState } from "react";
+import { dummyCourses } from "../assets/assets";
+
+ export const AppContext = createContext();
+
+ export const AppContextProvider = (props) => {
+    const currency = import.meta.env.VITE_CURRENCY;
+    const [allcourses, setAllCourses] = useState([]);
+    const fetchAllCourses = async () => {
+         setAllCourses(dummyCourses)
+    };
+
+    useEffect(()=>{
+        fetchAllCourses();
+    },[]);
+
+    const value = {currency,allcourses};
+   return <AppContext.Provider value={value}>{props.children}</AppContext.Provider>;
+ }
+
